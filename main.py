71750cfc7ea7a1967006598551a4d5b35690d5f8
Thyago7171 <thyagooliveira77@hotmail.com>
2023-02-17 05:27:43 -0300

    adição de comentários

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from time import sleep
 
-tam_linha = 20
-quant_linhas = 30
-passo = 7
-fundo = '0'
-caractere = '1'
-tempo = 0.05
-
+tam_linha = 20  # define o tamanho de cada linha do desenho
+passo = 7  # define quanto o pincel deve se deslocar entre as aparições
+fundo = '0'  # define o caracter que representará o fundo do quadro
+caractere = '1'  # define o caracter que represetará a tinta do pincel
+tempo = 0.05  # define a velocidade com a qual o desenho se move
 
 regra = passo
 while True:
-    for d in range(1, tam_linha + 1):
+    for contador in range(1, tam_linha + 1):
         regra += 1
         if regra % passo == 0:
             print(caractere, end='')
