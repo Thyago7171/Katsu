394cbf1b66a29eb6a4a718a6bb406845428c3409
Thyago7171 <thyagooliveira77@hotmail.com>
2023-02-21 05:03:51 -0300

    adição e correção de comentários

--- main.py
+++ main.py
@@ -3,36 +3,43 @@ import os
 from time import sleep
 
 
+# Altera a quantidade de caracteres por linha
 def lenght():
     global tam_linha
     tam_linha = int(caixa_01.get())
 
 
+# Altera a quantidade de linhas
 def height():
     global quant_linha
     quant_linha = int(caixa_02.get())
 
 
+# Altera o espaçamento entre a variação de caracter utilizado
 def step():
     global passo
     passo = int(caixa_03.get())
 
 
+# Altera o caracter padrão
 def background():
     global fundo
     fundo = caixa_04.get()
 
 
+# Altera o caracter especial
 def char():
     global caractere
     caractere = caixa_05.get()
 
 
+# Altera o tempo de sleep
 def speed():
     global tempo
-    tempo = 0.05 / float(caixa_06.get())
+    tempo = 0.05 / float(caixa_06.get())  # Calcula o tempo inversamente proporcional à velocidade
 
 
+# Executa o print resultado dos valores escolhidos
 def play():
     global tam_linha
     global quant_linha
@@ -53,6 +60,7 @@ def play():
         sleep(tempo)
 
 
+# Limpa o terminal
 def clean():
     os.system('cls')
 
@@ -73,17 +81,17 @@ tempo = 0.05  # define a velocidade com a qual o desenho se move
 # --------------------------------
 # WIDGETS
 
-caixa_01 = Entry(janela)  # adicionar valor padrão "20"
+caixa_01 = Entry(janela)
 caixa_01.insert(END, "20")
-caixa_02 = Entry(janela)  # adicionar valor padrão "100"
+caixa_02 = Entry(janela)
 caixa_02.insert(END, "100")
-caixa_03 = Entry(janela)  # adicionar valor padrão "7"
+caixa_03 = Entry(janela)
 caixa_03.insert(END, "7")
-caixa_04 = Entry(janela)  # adicionar valor padrão "0"
+caixa_04 = Entry(janela)
 caixa_04.insert(END, "0")
-caixa_05 = Entry(janela)  # adicionar valor padrão "1"
+caixa_05 = Entry(janela)
 caixa_05.insert(END, "1")
-caixa_06 = Entry(janela)  # adicionar valor padrão "1"
+caixa_06 = Entry(janela)
 caixa_06.insert(END, "1")
 
 
@@ -92,12 +100,12 @@ botao_02 = Button(janela, text="Alterar a altura do quadro", command=height)
 botao_03 = Button(janela, text="Alterar a pincelada", command=step)
 botao_04 = Button(janela, text="Alterar a aparência do quadro", command=background)
 botao_05 = Button(janela, text="Alterar a tinta", command=char)
-botao_06 = Button(janela, text="Alterar a velocidade da obra", command=speed)  # speed = tempo * 0.05
+botao_06 = Button(janela, text="Alterar a velocidade da obra", command=speed)
 botao_07 = Button(janela, text="Pintar", command=play)
 botao_08 = Button(janela, text="Limpar o quadro", command=clean)
 
 # --------------------------------
-# Layout
+# LAYOUT
 
 caixa_01.grid(row=0, column=0, pady=5)
 botao_01.grid(row=0, column=1, pady=5)
